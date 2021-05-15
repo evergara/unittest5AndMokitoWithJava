4d235579768856ada23929fdacada4737a3bbcfa
emersonvergara <emerson.vergara@ceiba.com.co>
2021-05-15 14:36:14 -0500

    Se le agrega a la clase Test dos metodos narcado con las etiqueta @BeforeEach para inicializar recurso y @AfterEach para liberal recurso

--- src/test/java/calculator/calculatorTest.java
+++ src/test/java/calculator/calculatorTest.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,14 +11,23 @@ class CalculatorTest {
 
     Calculator calculator;
 
-    @Test
-    public void calculatorISNullTest(){
-        assertNull(calculator, "Calculator debe ser Null");
+    @BeforeEach
+    public void setUp(){
+        calculator = new Calculator();
+        System.out.println("@BeforeEach -> setUp()");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        calculator = null;
+        System.out.println("@AfterEach -> tearDown()");
     }
 
     @Test
     public void calculatorNotNullTest(){
-        calculator = new Calculator();
         assertNotNull(calculator, "Calculator debe estar Instanciado y no ser Null");
+        System.out.println(" @Test -> calculatorNotNullTest()");
     }
+
+
 }
